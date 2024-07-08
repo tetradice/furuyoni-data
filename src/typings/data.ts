b39b9ba8db67d8f42ed71b2ceaf7837b1a80ad4d
Dice <tetradice@gmail.com>
2024-07-09 00:48:00 +0900

    シーズン9-2対応途中

--- src/typings/data.ts
+++ src/typings/data.ts
@@ -15,7 +15,8 @@ export type StandardCardSet =
   | "na-s7-2"
   | "na-s8"
   | "na-s8-2"
-  | "na-s9";
+  | "na-s9"
+  | "na-s9-2";
 
 /** 物語カードセット（はじまりの決闘、メガミへの挑戦も含む） */
 export type StoryCardSet =
